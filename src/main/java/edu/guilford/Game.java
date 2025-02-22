dd2c54c40ecc7104c3638870a9e19d72306e4f5e
Z-M-Man <112212530+Z-M-Man@users.noreply.github.com>
2025-02-21 22:06:01 -0500

    Create Game class

---
+++ src/main/java/edu/guilford/Game.java
@@ -0,0 +1,52 @@
+package edu.guilford;
+
+import java.util.*;
+
+public class Game {
+    private Random random = new Random();
+    private List<Player> players;
+    private Deck deck;
+    private Stack<Card> discard;
+    private Queue<Card> stockpile;
+
+    public Game() {
+        this.players = new ArrayList<>();
+        this.deck = new Deck();
+        this.discard = new Stack<>();
+        this.stockpile = new LinkedList<>();
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public Stack<Card> getDiscard() {
+        return discard;
+    }
+
+    public void setDiscard(Stack<Card> discard) {
+        this.discard = discard;
+    }
+
+    public Queue<Card> getStockpile() {
+        return stockpile;
+    }
+
+    public void setStockpile(Queue<Card> stockpile) {
+        this.stockpile = stockpile;
+    }
+
+    
+}
